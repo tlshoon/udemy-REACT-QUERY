9d3de465bdbb0c2764fa5bf97904e66366fcdfab
신재훈 <sjhh0105@naver.com>
2023-08-05 22:55:30 +0900

    feat: 블로그 쿼리 뮤테이션

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,21 +1,54 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
 import { PostDetail } from "./PostDetail";
+import { useQuery, useQueryClient } from "react-query";
+
 const maxPostPage = 10;
 
-async function fetchPosts() {
+const fetchPosts = async (pageNum) => {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
   return response.json();
-}
+};
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery(["posts", nextPage], () =>
+        fetchPosts(nextPage)
+      );
+    }
+  }, [currentPage, queryClient]);
+
   // replace with useQuery
-  const data = [];
+  /**
+   * useQuery('쿼리키','쿼리함수', '만료시간')
+   * 쿼리함수는 비동기여야함
+   * 만료가 된 후에만 리페치를 함
+   */
+  const { data, isError, error, isLoading } = useQuery(
+    ["posts", currentPage],
+    () => fetchPosts(currentPage),
+    {
+      staleTime: 2000,
+      keepPreviousData: true,
+    }
+  );
+  if (isLoading) return <h3>Loading...</h3>;
+
+  if (isError)
+    return (
+      <>
+        <h3>오류 발생!!!!</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
 
   return (
     <>
@@ -31,11 +64,17 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => setCurrentPage((previousValue) => previousValue - 1)}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => setCurrentPage((previousValue) => previousValue + 1)}
+        >
           Next page
         </button>
       </div>
